91bff3544c3d6e4b67653a87155cff66ad2e94d4
MingWang <hughes53@eattea.uk>
2025-08-31 19:01:44 +0800

    Add files via upload

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,15 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
 import { userService } from "@/services/userService";
+import { IdentityService } from "@/app/services/identityService";
 import type { User } from "@/app/types";
 import { userSignal } from "@/signals/userSignal";
 export default function useUser(country: string) {
+  const identityService = new IdentityService();
+
   const userQuery = useQuery<User, Error>({
     queryKey: ["user", country],
     queryFn: async () => {
       console.log("用户请求发起");
-      const response = await userService.fetchUser(country);
-      userSignal.value = response;
-      return response;
+      const baseUser = await userService.fetchUser(country);
+
+      // 增强用户信息，添加新的身份字段
+      const enhancedUser = identityService.enhanceUser(baseUser);
+
+      userSignal.value = enhancedUser;
+      return enhancedUser;
     },
     enabled: !!country,
     refetchOnWindowFocus: false, // 在窗口重新聚焦时不要重新获取数据
